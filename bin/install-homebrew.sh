d2ec7aa5caf85bfce3dfe8c830a95aeda566736c
Landon Bouma <landonb@users.noreply.github.com>
2024-11-05 21:01:29 -0600

    Update: Remove LibreOffice ~/.local/bin/ symlinks
    - From an Alacritty terminal, the aliases would open
      LibreOffice with the Alacritty menubar, what gives.
    
    - So replacing with aliases from Bashrc to compensate.
    
      - REFER: See new aliases in DepoXy:
    
        https://github.com/DepoXy/depoxy/blob/1.3.13/core/aliastown.sh#L329-L355

--- bin/install-homebrew.sh
+++ bin/install-homebrew.sh
@@ -645,9 +645,6 @@ BREW_APPS+=("--cask yed")
 
 # "Free cross-platform office suite, fresh version"
 BREW_APPS+=("--cask libreoffice")
-_MOSON_SOFFICE="/Applications/LibreOffice.app/Contents/MacOS/soffice"
-POST_EVAL+=("command ln -sfn '${_MOSON_SOFFICE}' '${HOME}/.local/bin/libreoffice'")
-POST_EVAL+=("command ln -sfn '${_MOSON_SOFFICE}' '${HOME}/.local/bin/soffice'")
 
 # --------------------------
 
